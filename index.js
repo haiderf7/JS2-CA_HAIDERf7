5d49bf34bb4c81b89d400a62d0501e6f4572bb56
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2023-05-04 12:21:51 +0200

    new

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function generateHtml(data) {
   const items = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
   data.forEach(function (item) {
-    const isItemInLocalStorage = items.includes(item.id);
+    const isItemInLocalStorage = items.some ( (storageItem) => storageItem.id === item.id);
 
     displayContainer.innerHTML += `
       <div>
         <div>Post item ${item.id}</div>
         <div>${item.title}</div>
         <div>${item.summary}</div>
-        <button data-id="${item.id}" data-in-local-storage="${isItemInLocalStorage}">${isItemInLocalStorage ? 'Remove' : 'Save'}</button>
+        <button data-id="${item.id}"data-title="${item.title}" data-in-local-storage="${isItemInLocalStorage}">${isItemInLocalStorage ? 'Remove' : 'Save'}</button>
       </div>
       <hr />
     `;
@@ -27,16 +27,17 @@ function generateHtml(data) {
 
   displayContainer.addEventListener('click', function(event) {
     if (event.target.tagName === 'BUTTON') {
-      const itemId = parseInt(event.target.dataset.id);
+      const itemID = parseInt(event.target.dataset.id);
+      const iteTitle = event.target.dataset.title;
       const isInLocalStorage = event.target.dataset.inLocalStorage === 'true';
 
       if (isInLocalStorage) {
-        const index = items.indexOf(itemId);
+        const index = items.findIndex ( (item) => item.id === itemId);
         if (index !== -1) {
           items.splice(index, 1);
         }
       } else {
-        items.push(itemId);
+        items.push({ id: itemId, title: itemTitle });
       }
 
       localStorage.setItem(localStorageKey, JSON.stringify(items));
